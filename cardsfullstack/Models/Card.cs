6314f6331dad34fde364247240cc56ffae66c8f1
Metzner <jeffmetzner@rocketmortgage.com>
2021-11-03 11:27:30 -0400

    Added model classes

---
+++ cardsfullstack/Models/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cardsfullstack.Models
+{
+    public class Card
+    {
+        public int id { get; set; }
+        public string deck_id { get; set; }
+        public string image { get; set; }
+        public string cardcode { get; set; }
+        public string username { get; set; }
+        public DateTime created_at { get; set; }
+
+    }
+}
---
+++ cardsfullstack/Models/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cardsfullstack.Models
+{
+    public class Deck
+    {
+        public string deck_id { get; set; }
+        public bool is_current { get; set; }
+        public DateTime created_at { get; set; }
+        public string username { get; set; }
+
+    }
+}
